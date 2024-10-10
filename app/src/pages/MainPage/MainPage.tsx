2b204fdfb55790117e43900e078b27c5e4bbbd92
Pirushov Maxim <maximalka111@yandex.ru>
2024-10-10 21:59:38 +0400

    added footer header meta

--- app/src/pages/MainPage/MainPage.tsx
+++ app/src/pages/MainPage/MainPage.tsx
@@ -2,6 +2,9 @@ import * as css from './style.module.scss'
 export const MainPage = () => {
     const hobbies = ['playing football', 'reading books', 'walking down the street']
     return (
+        <>
+        
+        <header>Я Хедер!</header>
         <div className={css.mainInformation}>
             <h1>Hello,  my name is Maxim!</h1>
             <p>My hobbies are</p>
@@ -15,5 +18,7 @@ export const MainPage = () => {
             <p>Of course I love dogs. Here is a photo of a pretty dog.</p>
             <img src='https://thumbs.dreamstime.com/b/beautiful-happy-reddish-havanese-puppy-dog-sitting-frontal-looking-camera-isolated-white-background-46868560.jpg' alt='photo of dog'/>
         </div>
+        <footer>Здесь расположена контактная информация</footer>
+        </>
     );
 };
